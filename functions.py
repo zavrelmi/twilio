e48740852162769eac9fad8bf1b19fbb8b7d4384
zavrelmi <zavrelmi@gmail.com>
2022-08-18 15:18:00 -0500

    dictionaries and functions solution

---
+++ functions.py
@@ -0,0 +1,13 @@
+'''
+Define a function called hail_friend which prints the message Hail, 
+friend! to the console. 
+This will be your first step to learning the basic moving parts of Python functions. 
+Once you have defined your function in functions.py, 
+click the HACK button to complete the trial.
+'''
+
+def hail_friend():
+    print("Hail, friend!")
+
+if __name__ == '__main__':
+    hail_friend()
